c7d96c49dab2021c4d0336fc35350252363ae1a1
barkum <discordloginn123@gmail.com>
2025-01-22 18:13:15 +0400

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using q3;
+
+var serviceProvider = new ServiceCollection().AddDbContext<SchoolContext>(options => options.UseInMemoryDatabase("SchoolDB")).BuildServiceProvider();
+
+using (var scope = serviceProvider.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<SchoolContext>();
+
+    context.Database.EnsureCreated();
+
+    var studentManager = new StudentManager(context);
+
+    studentManager.AddStudent(new Student {Id = 5, Name = "test", Grade = 23});
+
+    var students = studentManager.GetAllStudents();
+
+
+    foreach (var student in students)
+
+    {
+        Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Grade: {student.Grade}");
+    }
+}
