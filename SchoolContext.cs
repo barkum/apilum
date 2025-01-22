c88961d98ac77a2b9394f2adaf6261254a9470f1
barkum <discordloginn123@gmail.com>
2025-01-22 18:13:53 +0400

    Add files via upload

---
+++ SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace q3
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().HasData(
+                new Student { Id = 1, Name = "John", Grade = 5 },
+                new Student { Id = 2, Name = "Get", Grade = 11 }
+                );
+        }
+
+    }
+}
